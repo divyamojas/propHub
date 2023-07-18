4a8ec318db7235e15f9b0b2e292bfdd0b5cb5031
divyamojas <divyamojas@gmail.com>
2023-07-18 08:52:30 +0530

    remove redundant bid property page component

--- client/src/pages/BidProperty.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BidProperty = () => {
-  return (
-    <div>BidProperty</div>
-  )
-}
-
-export default BidProperty
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,5 @@
 export { default as Home } from './Home'
 export { default as Profile } from './Profile'
 export { default as ListProperty } from './ListProperty'
-export { default as BidProperty } from './BidProperty'
 export { default as MyActivities } from './MyActivities'
 export { default as PropertyDetails } from './PropertyDetails'
