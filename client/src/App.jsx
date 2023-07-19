05c152f9ea2adcf196955a1c02101df6892e0f80
divyamojas <divyamojas@gmail.com>
2023-07-19 10:23:46 +0530

    Add 'Add Network' if metamask not connected to my network

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { Home, ListProperty, PropertyDetails, MyActivities } from "./pages";
@@ -6,6 +6,66 @@ import { Sidebar, Navbar } from "./components";
 
 const App = () => {
   console.clear();
+
+  useEffect(() => {
+    // Function to check if MetaMask is connected to the required network
+    const checkMetaMaskConnection = async () => {
+      // Check if MetaMask is installed and connected
+      if (typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask) {
+        // const requiredNetworkId = '1337'; // Network ID of the required network
+        const requiredChainId = '0x539'; // Chain ID of the required network
+        const requiredRpcUrl = 'HTTP://127.0.0.1:7545'; // RPC URL of the required network
+        const requiredCurrency = 'ETH'; // Required currency symbol
+
+        try {
+          // Get the network ID from MetaMask
+
+          // Get the chain ID from MetaMask
+          const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+
+          // Check if MetaMask is connected to the required network
+          if (/*parseInt(networkId) === requiredNetworkId && */chainId === requiredChainId) {
+            console.log('MetaMask is connected to the required network');
+          } else {
+            // Prompt user to connect to the required network
+            await window.ethereum.request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainId: requiredChainId,
+                  chainName: 'Loc_GUI',
+                  rpcUrls: [requiredRpcUrl],
+                  nativeCurrency: {
+                    name: 'Ether',
+                    symbol: requiredCurrency,
+                    decimals: 18,
+                  },
+                },
+              ],
+            }).then((el) => console.log(el));
+            console.log('Connected to the required network');
+          }
+        } catch (error) {
+          console.error('Error checking MetaMask connection:', error);
+        }
+      } else {
+        console.error('MetaMask is not installed or not connected');
+      }
+    };
+
+    // Check MetaMask connection on app load
+    checkMetaMaskConnection();
+
+    // Rest of your existing code for handling accounts and address
+
+    // Cleanup function to remove the 'accountsChanged' event listener
+    return () => {
+      if (typeof window.ethereum !== 'undefined') {
+        window.ethereum.removeAllListeners('accountsChanged');
+      }
+    };
+  }, []); // Add 'address' as a dependency if it's used in the existing code
+
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
